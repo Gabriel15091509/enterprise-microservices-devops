2d5a597c3b4be0c555da7673d00a14a2a1bba16f
Gabriel <lovanomeny72@gmail.com>
2026-03-18 10:22:07 +0300

    lova

--- services/product-service/src/routes/product.routes.js
+++ services/product-service/src/routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middleware/auth.middleware");
 
-router.get("/products", verifyToken, (req, res) => {
+router.get("/products", (req, res) => {
    return res.json([
         { id: 1, name: "Laptop" },
         { id: 2, name: "Phone" }
